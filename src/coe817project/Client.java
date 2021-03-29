5c032677f7f9b85fb2d74b8d1abd05e021d3400f
jozifmikhael <jozifmikhael@gmail.com>
2021-03-29 18:43:32 -0400

    Added client and supervisor communication

--- src/coe817project/Client.java
+++ src/coe817project/Client.java
@@ -1,68 +1,147 @@
 package coe817project;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import java.beans.Encoder;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
 import java.net.Socket;
-import java.security.InvalidKeyException;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.PrivateKey;
 import java.security.KeyPair;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+import java.util.Date;
+import java.util.Scanner;
+import java.text.SimpleDateFormat;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
 
 
 public class Client {
-	Cipher c_rCipher;
-	PublicKey publicK_A, publicK_B;
-    PrivateKey privateK_A;
-    KeyPair keyPair_A;
-	KeyPairGenerator keyPairGen_A;
-    int Socket_Port = 10001;                     
-    String IDa = "Initiator A";
-    String sessionKey = "SESSION KEY SUCCESSFUL";
-	private Socket socket;
-	private BufferedReader input;
-    private DataOutputStream out;
-    private DataInputStream in;
-    
-	public Client(String address, int port) throws InvalidKeyException, NoSuchPaddingException {
-		try {
-            socket = new Socket(address, port);
-            System.out.println("Initiator A Connected" +"\n");
-
-            //ready the input reader
-            input = new BufferedReader(new InputStreamReader(System.in));
-            in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
-            out = new DataOutputStream(socket.getOutputStream());
-
-            String messagein = "";
-            String messageout = "";
-		} catch (IOException i) {
-            System.out.println(i);
-        } 
-//            catch (NoSuchAlgorithmException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IllegalBlockSizeException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (BadPaddingException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+	
+	public static void main(String[] args) throws IOException {
+        
+		Cipher c_rCipher;
+		PublicKey publicK_A, publicK_B;
+        PrivateKey privateK_A;
+        KeyPair keyPair_A;
+		KeyPairGenerator keyPairGen_A;
+        int Socket_Port = 10001;                     
+        String IDa = "Initiator A";
+        String sessionKey; //= "ARANDOMEVALUE";
+        
+        System.out.println("====================");
+        System.out.println("| Payment Terminal |");
+        System.out.println("====================");
+        
+        Socket ss = new Socket("localhost", Socket_Port);        
+        Scanner serverInput = new Scanner(ss.getInputStream());
+        Scanner userInput = new Scanner(System.in);      
+        System.out.println("Successfully Connected to Seller's Terminal");
+        PrintStream printStr = new PrintStream(ss.getOutputStream());        
+        
+        try{
+            keyPairGen_A = KeyPairGenerator.getInstance("RSA");
+            keyPair_A = keyPairGen_A.generateKeyPair();
+            publicK_A = keyPair_A.getPublic();
+            privateK_A = keyPair_A.getPrivate();            
+            String publicKeyString_B = serverInput.nextLine();
+            byte [] key_ByteCodeB = Base64.getDecoder().decode(publicKeyString_B);
+            X509EncodedKeySpec X509KeySpec = new X509EncodedKeySpec(key_ByteCodeB);
+            publicK_B = KeyFactory.getInstance("RSA").generatePublic(X509KeySpec);            
+            c_rCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            c_rCipher.init(Cipher.ENCRYPT_MODE,publicK_B);
+                                
+            SimpleDateFormat dateFormat2 = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+            String A_Date = dateFormat2.format(new Date());            
+            byte[] c_iDAByteCode = c_rCipher.doFinal(IDa.getBytes("UTF-8"));
+            byte[] c_NAByteCode = c_rCipher.doFinal(A_Date.getBytes("UTF-8"));        
+            
+            String enc_IDa = Base64.getEncoder().encodeToString(c_iDAByteCode);
+            String enc_nA = Base64.getEncoder().encodeToString(c_NAByteCode);            
+            printStr.println(enc_IDa);
+            printStr.println(enc_nA);
+            String public_A = Base64.getEncoder().encodeToString(publicK_A.getEncoded());
+            printStr.println(public_A);            
+            String A_Nonce = serverInput.nextLine();
+            String B_Nonce = serverInput.nextLine();            
+            c_rCipher.init(Cipher.DECRYPT_MODE, privateK_A);
+            byte[] nA_ByteCode = Base64.getDecoder().decode(A_Nonce);
+            byte[] nB_ByteCode = Base64.getDecoder().decode(B_Nonce);
+            A_Nonce = new String(c_rCipher.doFinal(nA_ByteCode));
+            B_Nonce = new String(c_rCipher.doFinal(nB_ByteCode));
+//            System.out.println("Nonce A: " + A_Nonce);
+//            System.out.println("Nonce B: " + B_Nonce);            
+                     
+            c_rCipher.init(Cipher.ENCRYPT_MODE, publicK_B);
+            nB_ByteCode = c_rCipher.doFinal(B_Nonce.getBytes("UTF-8"));
+            String nB_Encrypted = Base64.getEncoder().encodeToString(nB_ByteCode);
+            printStr.println(nB_Encrypted);
+            
+            SecureRandom random = new SecureRandom();
+            byte bytes[] = new byte[20];
+            random.nextBytes(bytes);
+            //Encoder encoder = Base64.getEncoder().withoutPadding();
+            sessionKey = Base64.getEncoder().encodeToString(bytes);
+            System.out.println(sessionKey);
+            
+            byte[] SeshKey_ByteCode = c_rCipher.doFinal(sessionKey.getBytes("UTF-8"));
+            String enc_SKByteCode = Base64.getEncoder().encodeToString(SeshKey_ByteCode);
+            printStr.println(enc_SKByteCode);
+            
+            System.out.println("Please Enter your Credit Card Information Below: \n");
+            
+            System.out.println("Cardholder Name: ");
+            String cardHolderName = userInput.nextLine();            
+                        
+            System.out.println("Credit Card Number: ");            
+            String cardNumber = userInput.nextLine();  
+            
+            System.out.println("Expiry (MMYY): ");
+            String cardExp = userInput.nextLine();            
+                        
+            System.out.println("3 Digit CVV: ");            
+            String cardCVV = userInput.nextLine();  
+            
+            SecretKeyFactory SecKey_F2 = SecretKeyFactory.getInstance("DES");
+            DESKeySpec KeyDES_Bytes2 = new DESKeySpec(sessionKey.getBytes());
+            SecretKey SecKey_gen2 = SecKey_F2.generateSecret(KeyDES_Bytes2);
+            
+            String enc_cardHolderName = encrypt(cardHolderName, SecKey_gen2);
+            String enc_cardNumber = encrypt(cardNumber, SecKey_gen2);
+            String enc_cardExp = encrypt(cardExp, SecKey_gen2);
+            String enc_cardCVV = encrypt(cardCVV, SecKey_gen2);
+            
+            printStr.println(enc_cardHolderName); 
+            printStr.println(enc_cardNumber);
+            printStr.println(enc_cardExp); 
+            printStr.println(enc_cardCVV); 
+                                    
+            System.out.println("Credit Card Information has been Securely Transmitted to Seller");
+            
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
 	}
 	
-	public static void main(String args[]) throws InvalidKeyException, NoSuchPaddingException {
-		
-        Client buyer = new Client("127.0.0.1", 6666);
-    }
+	public static String encrypt(String encryptedMessage, SecretKey SecKey_gen) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException {
+		Cipher DES_Cipher2 = Cipher.getInstance("DES/ECB/PKCS5Padding");
+        DES_Cipher2.init(Cipher.ENCRYPT_MODE, SecKey_gen);            
+        
+        byte[] Msg_ByteCode = DES_Cipher2.doFinal(encryptedMessage.getBytes("UTF-8"));
+        
+        String enc_Msg = Base64.getEncoder().encodeToString(Msg_ByteCode);
+		return enc_Msg;
+	}
 }
--- src/coe817project/Supervisor.java
+++ src/coe817project/Supervisor.java
@@ -1,46 +1,140 @@
 package coe817project;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.text.SimpleDateFormat;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Base64;
+import java.util.Date;
+import java.util.Scanner;
+import java.security.PublicKey;
+import java.security.PrivateKey;
+import java.security.KeyPair;
 import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+
 
 public class Supervisor {
-	private DataOutputStream out;
-    private Socket socket;
-    private ServerSocket server;
-    private DataInputStream in;
-    
-    public Supervisor(int port) {
-    	try {
+	
+	public static void main(String[] args) throws IOException {
+        
+		Cipher rCipher;
+		PublicKey publicK_A, publicK_B;
+        PrivateKey privateK_B;   
+        KeyPair keyPair_B;
+        KeyPairGenerator keyPairGen_B;
+        int Socket_Port = 10001;
+        
+        String IDb = "Seller";
+
+        System.out.println("=====================");
+        System.out.println("| Seller's Terminal |");
+        System.out.println("=====================");        
+        ServerSocket ss = new ServerSocket(Socket_Port);        
+        System.out.println("Starting connection to Client's terminal on socket: " + Socket_Port);        
+        Socket ssc = ss.accept();    
+        System.out.println("Successfully Connected to Client");
+        Scanner clientIn = new Scanner(ssc.getInputStream());
+        PrintStream printStr = new PrintStream(ssc.getOutputStream());
+                       
+        try {
             
-            server = new ServerSocket(port);
-            System.out.println("Supervisor started");
-            System.out.println("Waiting for client ...");
+            keyPairGen_B = KeyPairGenerator.getInstance("RSA");
+            keyPair_B = keyPairGen_B.generateKeyPair();
+            publicK_B = keyPair_B.getPublic();
+            privateK_B = keyPair_B.getPrivate();            
+            String publicKeyString_B = Base64.getEncoder().encodeToString(publicK_B.getEncoded());
+            printStr.println(publicKeyString_B);            
+            rCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            rCipher.init(Cipher.DECRYPT_MODE, privateK_B);            
+            String client_IDa = clientIn.nextLine();
+            String client_nA = clientIn.nextLine();
+            byte[] c_byteCode = Base64.getDecoder().decode(client_IDa);
+            byte[] c_nAByteCode = Base64.getDecoder().decode(client_nA);
+            client_IDa = new String(rCipher.doFinal(c_byteCode));
+            client_nA = new String(rCipher.doFinal(c_nAByteCode));
+            System.out.println("Client's ID is: " + client_IDa + " -- with timestamp: -- " + client_nA );            
             
-            socket = server.accept();
-            System.out.println("Client accepted"+"\n");
+            String ServerPublicKey_A = clientIn.nextLine();
+            byte[] s_nAByteCode = Base64.getDecoder().decode(ServerPublicKey_A);
+            X509EncodedKeySpec X509_KeySpec = new X509EncodedKeySpec(s_nAByteCode);
+            publicK_A = KeyFactory.getInstance("RSA").generatePublic(X509_KeySpec);            
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+            String B_Date = dateFormat.format(new Date());            
+            rCipher.init(Cipher.ENCRYPT_MODE, publicK_A);
+            byte[] c_nAByteCode2 = rCipher.doFinal(client_nA.getBytes("UTF-8"));
+            byte[] c_nBByteCode2 = rCipher.doFinal(B_Date.getBytes("UTF-8"));
+            String enc_nA = Base64.getEncoder().encodeToString(c_nAByteCode2);
+            String enc_nB = Base64.getEncoder().encodeToString(c_nBByteCode2);            
+            printStr.println(enc_nA);
+            printStr.println(enc_nB);
             
-            in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
-            out = new DataOutputStream(socket.getOutputStream());     
+            rCipher.init(Cipher.DECRYPT_MODE, privateK_B);
+            String c_nB = clientIn.nextLine();
+            byte[] c_BDecByteCode = Base64.getDecoder().decode(c_nB);
+            c_nB = new String(rCipher.doFinal(c_BDecByteCode));
+            System.out.println("Received Timestamp: " + c_nB);            
+            String sesh_Key = clientIn.nextLine();
+            byte[] sesh_KeyByteCode = Base64.getDecoder().decode(sesh_Key);
+            String sesh_KeyByteDecode = new String(rCipher.doFinal(sesh_KeyByteCode));
+            System.out.println("Session Key: " + sesh_KeyByteDecode);            
             
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            String messagein = "";
-            String messageout = "";
+            String enc_cardHolderName = clientIn.nextLine();
+            System.out.println("Encrypted Cardholder Name: " + enc_cardHolderName);
             
-    	}catch(IOException i){
-            System.out.println(i);
+            String enc_cardNumber = clientIn.nextLine();
+            System.out.println("Encrypted Credit Card Number: " + enc_cardNumber);
+            
+            String enc_cardExp = clientIn.nextLine();
+            System.out.println("Encrypted Credit Card Expiry: " + enc_cardExp);
+            
+            String enc_cardCVV = clientIn.nextLine();
+            System.out.println("Encrypted Credit Card CVV Code: " + enc_cardCVV);
+            
+            SecretKeyFactory SecKey_F = SecretKeyFactory.getInstance("DES");
+            DESKeySpec KeyDES_Bytes = new DESKeySpec(sesh_KeyByteDecode.getBytes());
+            SecretKey SecKey_gen = SecKey_F.generateSecret(KeyDES_Bytes);
+            
+            String dec_Name = decrypt(enc_cardHolderName, SecKey_gen);
+            String dec_Number = decrypt(enc_cardNumber, SecKey_gen);
+            String dec_Exp = decrypt(enc_cardExp, SecKey_gen);
+            String dec_CVV = decrypt(enc_cardCVV, SecKey_gen);
+            
+            System.out.println("Decrypted Cardholder Name: " + dec_Name);
+            System.out.println("Decrypted Credit Card Number: " + dec_Number);
+            System.out.println("Decrypted Expiry Date in MMYY: " + dec_Exp);
+            System.out.println("Decrypted Credit Card CVV Code: " + dec_CVV);
+            
+            
         }
+        catch (Exception e)
+        {
+            System.out.println(e);
+        }        
     }
-    
-    public static void main(String args[]) throws NoSuchAlgorithmException, InvalidKeyException, InvalidKeySpecException{       
-        Supervisor initiator = new Supervisor(6666);
-    }
+
+	public static String decrypt(String encryptedMessage, SecretKey SecKey_gen) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+		String en_Msg = encryptedMessage;
+		Cipher DES_Cipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
+    	DES_Cipher.init(Cipher.DECRYPT_MODE, SecKey_gen);            
+    	byte[] decByteCode = Base64.getDecoder().decode(en_Msg);            
+    	byte[] deEncByte = DES_Cipher.doFinal(decByteCode);
+    	String dec_FinalMsg = new String(deEncByte);
+		return dec_FinalMsg;
+	}
+		
 }
+
+
